72cf688d95cd460a5dab8adc2fe344e0c813b4f4
Jose Bento <jldbento@gmail.com>
2023-12-15 20:28:36 -0300

    update_project8

--- src/Payments/Payments.java
+++ src/Payments/Payments.java
@@ -1,7 +1,7 @@
 package Payments;
 
 public class Payments {
-    public static void main(String[] args) {
+
 
         double pix;
         double money;
@@ -9,6 +9,55 @@ public class Payments {
         double credit;
         String voucher;
 
+    public Payments(double pix, double money, double debit, double credit, String voucher) {
+        this.pix = pix;
+        this.money = money;
+        this.debit = debit;
+        this.credit = credit;
+        this.voucher = voucher;
+
     }
 
+    public double getPix() {
+        return pix;
+    }
+
+    public void setPix(double pix) {
+        this.pix = pix;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public double getDebit() {
+        return debit;
+    }
+
+    public void setDebit(double debit) {
+        this.debit = debit;
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    public void setCredit(double credit) {
+        this.credit = credit;
+    }
+
+    public String getVoucher() {
+        return voucher;
+    }
+
+    public void setVoucher(String voucher) {
+        this.voucher = voucher;
+    }
 }
+
+
+
